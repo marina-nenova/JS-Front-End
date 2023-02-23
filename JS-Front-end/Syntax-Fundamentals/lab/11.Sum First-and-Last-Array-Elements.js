e3b4bb79a4832aa92a318a6640ba059265efee58
Marina Nenova <marinanenova89@gmail.com>
2023-02-23 16:52:22 +0200

    Solved Sum First and Last Array Elements exercise

---
+++ JS-Front-end/Syntax-Fundamentals/lab/11.Sum First-and-Last-Array-Elements.js
@@ -0,0 +1,9 @@
+function solve(array) {
+  let firtst = array[0];
+  let last = array[array.length - 1];
+  console.log(firtst + last);
+}
+
+solve([20, 30, 40]);
+solve([10, 17, 22, 33]);
+solve([11, 58, 69]);
