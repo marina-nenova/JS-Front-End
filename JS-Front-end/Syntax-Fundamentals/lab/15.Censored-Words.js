a624c33de3c87a34853607ea7efc29c6603a41d4
Marina Nenova <marinanenova89@gmail.com>
2023-02-23 17:14:10 +0200

    Solved Censored Words exercise

---
+++ JS-Front-end/Syntax-Fundamentals/lab/15.Censored-Words.js
@@ -0,0 +1,9 @@
+function solve(text, searchedWord) {
+  while (text.includes(searchedWord)) {
+    text = text.replace(searchedWord, "*".repeat(searchedWord.length));
+  }
+  console.log(text);
+}
+
+solve("A small sentence with some words small", "small");
+solve("Find the hidden word", "hidden");
