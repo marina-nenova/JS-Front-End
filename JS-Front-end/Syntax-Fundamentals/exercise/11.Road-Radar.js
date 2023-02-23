715df6e3912652746140e02b1ee6b9aee409de90
Marina Nenova <marinanenova89@gmail.com>
2023-02-23 19:42:05 +0200

    Solved Road Radar exercise

---
+++ JS-Front-end/Syntax-Fundamentals/exercise/11.Road-Radar.js
@@ -0,0 +1,40 @@
+function solve(speed, area) {
+  let speedLimit;
+
+  switch (area) {
+    case "motorway":
+      speedLimit = 130;
+      break;
+    case "interstate":
+      speedLimit = 90;
+      break;
+    case "city":
+      speedLimit = 50;
+      break;
+    case "residential":
+      speedLimit = 20;
+      break;
+  }
+  let difference = speedLimit - speed;
+
+  if (difference >= 0) {
+    console.log(`Driving ${speed} km/h in a ${speedLimit} zone`);
+  } else {
+    let status;
+    if (Math.abs(difference) <= 20) {
+      status = "speeding";
+    } else if (Math.abs(difference) <= 40) {
+      status = "excessive speeding";
+    } else {
+      status = "reckless driving";
+    }
+    console.log(
+      `The speed is ${Math.abs(difference)} km/h faster than the allowed speed of ${speedLimit} - ${status}`
+    );
+  }
+}
+
+solve(40, "city");
+solve(21, "residential");
+solve(120, "interstate");
+solve(200, "motorway");
