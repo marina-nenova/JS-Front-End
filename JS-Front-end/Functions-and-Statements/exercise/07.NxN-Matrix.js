aaf03e3f0d063cac9bed102b2b82a57074584b1f
Marina Nenova <marinanenova89@gmail.com>
2023-02-28 14:40:32 +0200

    Solved NxN Matrix exercise

---
+++ JS-Front-end/Functions-and-Statements/exercise/07.NxN-Matrix.js
@@ -0,0 +1,16 @@
+function matrixBuilder(n) {
+  let matrix = [];
+
+  for (let i = 0; i < n; i++) {
+    let row = [];
+    for (let j = 0; j < n; j++) {
+      row.push(n);
+    }
+    matrix.push(row);
+  }
+
+  matrix.forEach((row) => console.log(...row));
+}
+
+matrixBuilder(3);
+matrixBuilder(7);
