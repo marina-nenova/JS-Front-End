0e734791a26e97fdaadb99eabf601019fb66f419
Marina Nenova <marinanenova89@gmail.com>
2023-02-28 14:35:53 +0200

    Solved Password Validator exercise

---
+++ JS-Front-end/Functions-and-Statements/exercise/06.Password-Validator.js
@@ -0,0 +1,34 @@
+function passwordValidator(password) {
+  let validPassowrd = true;
+
+  function checkLength(password) {
+    if (password.length < 6 || password.length > 10) {
+      console.log("Password must be between 6 and 10 characters");
+      validPassowrd = false;
+    }
+  }
+  function checkCharacters(password) {
+    if (!/^[A-Za-z0-9]*$/.test(password)) {
+      console.log("Password must consist only of letters and digits");
+      validPassowrd = false;
+    }
+  }
+  function checkForTwoDigits(password) {
+    if (!/(\D*\d){2,}/.test(password)) {
+      console.log("Password must have at least 2 digits");
+      validPassowrd = false;
+    }
+  }
+
+  checkLength(password);
+  checkCharacters(password);
+  checkForTwoDigits(password);
+  
+  if (validPassowrd) {
+    console.log("Password is valid");
+  }
+}
+
+passwordValidator('logIn')
+passwordValidator('MyPass123')
+passwordValidator("Pa$s$s");
