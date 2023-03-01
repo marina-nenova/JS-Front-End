817986b1713c8e67738b0390c84556c896c90abc
Marina Nenova <marinanenova89@gmail.com>
2023-03-01 13:32:24 +0200

    solved Number Modification exercise

---
+++ JS-Front-end/Functions-and-Statements/more-exercise/02.Number-Modification.js
@@ -0,0 +1,20 @@
+function numberModifier(number) {
+    let numAsArr = number.toString().split('');
+    let averageSum = average(numAsArr);
+    while (averageSum < 5) {
+        numAsArr.push('9')
+        averageSum = average(numAsArr);
+    }
+    function average(numsArr) {
+        let sum = 0;
+        numsArr.forEach(digit => {
+            sum += Number(digit)
+        });
+        return sum / numAsArr.length
+    }
+    
+    console.log(numAsArr.join(''));
+}
+
+numberModifier(101);
+numberModifier(5835);
