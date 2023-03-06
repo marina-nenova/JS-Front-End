b3daded0f95e8404c7bd409d36ede0fab7c00f91
Marina Nenova <marinanenova89@gmail.com>
2023-03-06 14:48:36 +0200

    Solved City exercise

---
+++ JS-Front-end/Objects-and-Classes/lab/02.City.js
@@ -0,0 +1,23 @@
+function city(cityObject) {
+    Object.keys(cityObject).forEach(x => {
+        console.log(`${x} -> ${cityObject[x]}`);
+    })
+}
+
+city({
+    name: "Sofia",
+    area: 492,
+    population: 1238438,
+    country: "Bulgaria",
+    postCode: "1000"
+}
+);
+
+city({
+    name: "Plovdiv",
+    area: 389,
+    population: 1162358,
+    country: "Bulgaria",
+    postCode: "4000"
+}
+);
