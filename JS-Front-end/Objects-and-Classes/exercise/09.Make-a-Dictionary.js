75cbfd8a00fafdb161e694d7ea48d0450e6782bc
Marina Nenova <marinanenova89@gmail.com>
2023-03-09 15:26:53 +0200

    Solved Make a Dictionary exercise

---
+++ JS-Front-end/Objects-and-Classes/exercise/09.Make-a-Dictionary.js
@@ -0,0 +1,30 @@
+function dictMaker(jsonArr) {
+    let dictionary = {}
+    for (const data of jsonArr) {
+        let parsedData = JSON.parse(data);
+        let key = Object.keys(parsedData)[0];
+        let value = Object.values(parsedData)[0];
+        dictionary[key] = value;
+    }
+
+    for (item of Object.entries(dictionary).sort(([keyA], [keyB]) => keyA.localeCompare(keyB))){
+        console.log(`Term: ${item[0]} => Definition: ${item[1]}`)
+    }
+}
+
+dictMaker([
+    '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+    '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+    '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+    '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+    '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}'
+    ])
+
+dictMaker([
+    '{"Cup":"A small bowl-shaped container for drinking from, typically having a handle"}',
+    '{"Cake":"An item of soft sweet food made from a mixture of flour, fat, eggs, sugar, and other ingredients, baked and sometimes iced or decorated."} ',
+    '{"Watermelon":"The large fruit of a plant of the gourd family, with smooth green skin, red pulp, and watery juice."} ',
+    '{"Music":"Vocal or instrumental sounds (or both) combined in such a way as to produce beauty of form, harmony, and expression of emotion."} ',
+    '{"Art":"The expression or application of human creative skill and imagination, typically in a visual form such as painting or sculpture, producing works to be appreciated primarily for their beauty or emotional power."} '
+    ]
+    )
