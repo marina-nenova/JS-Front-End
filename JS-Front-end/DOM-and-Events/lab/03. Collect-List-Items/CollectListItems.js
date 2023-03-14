7ca8285ef9e64dad3c5bf49f7245e83da1e60280
Marina Nenova <marinanenova89@gmail.com>
2023-03-14 15:37:43 +0200

    Solved Collect List Items exercise

--- JS-Front-end/DOM-and-Events/lab/03. Collect-List-Items/CollectListItems.js
+++ JS-Front-end/DOM-and-Events/lab/03. Collect-List-Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+    let items = document.getElementsByTagName('li');
+    let resultElement = document.getElementById('result')
+    let itemsArr = Array.from(items);
+
+    resultElement.value = itemsArr.map((e) => e.textContent).join('\n');
+
 }
