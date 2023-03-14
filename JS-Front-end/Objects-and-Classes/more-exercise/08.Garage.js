f30268513668c06914dd34f84dc5f5657ca6e61a
Marina Nenova <marinanenova89@gmail.com>
2023-03-14 13:06:45 +0200

    revert garage exercise changes

--- JS-Front-end/Objects-and-Classes/more-exercise/08.Garage.js
+++ JS-Front-end/Objects-and-Classes/more-exercise/08.Garage.js
@@ -1,75 +1,41 @@
-// function garage(arr) {
-//   let garageStore = {};
+function garage(arr) {
+  let garageStore = {};
 
-//   for (const line of arr) {
-//     let [garageNum, carInfo] = line.split(" - ");
-//     let carDetails = carInfo.split(", ");
+  for (const line of arr) {
+    let [garageNum, carInfo] = line.split(" - ");
+    let carDetails = carInfo.split(", ");
 
-//     if (!garageStore[garageNum]) {
-//       garageStore[garageNum] = [];
-//     }
-
-//     let car = {};
-
-//     for (const carDetail of carDetails) {
-//       let [feature, desc] = carDetail.split(": ");
-//       car[feature] = desc;
-//     }
-//     garageStore[garageNum].push(car);
-//   }
-
-//   for (const garage in garageStore) {
-//     console.log(`Garage № ${garage}`);
-//     for (car of garageStore[garage]) {
-//       let desc = [];
-//       for (const prop in car) {
-//         desc.push(`${prop} - ${car[prop]}`);
-//       }
-//       console.log(`--- ${desc.join(", ")}`);
-//     }
-//   }
-// }
-
-function garage(array) {
-    let garageObject = {};
-    for (let index = 0; index < array.length; index++) {
-      const element = array[index];
-      let [garageId, carInfo] = element.split(" - ");
-
-      if (!garageObject.hasOwnProperty(garageId)) {
-        garageObject[garageId] = [];
-      }
-      const carTokens = carInfo
-        .split(", ")
-        .map((x) => x.split(": "));
-      const carObject = {};
-      for (const [key, value] of carTokens) {
-        carObject[key] = value;
-      }
-      garageObject[garageId].push(carObject);
+    if (!garageStore[garageNum]) {
+      garageStore[garageNum] = [];
     }
-    print(garageObject);
-   
-    function print(garageObject) {
-      for (const garageId of Object.keys(garageObject)) {
-        console.log(`Garage № ${garageId}`);
-        for (const car of garageObject[garageId]) {
-   
-          const carInformation = Object.entries(car)
-            .map(([key, value]) => `${key} - ${value}`)
-            .join(", ");
-          console.log(`--- ${carInformation}`);
-        }
+
+    let car = {};
+
+    for (const carDetail of carDetails) {
+      let [feature, desc] = carDetail.split(": ");
+      car[feature] = desc;
+    }
+    garageStore[garageNum].push(car);
+  }
+
+  for (const garage in garageStore) {
+    console.log(`Garage № ${garage}`);
+    for (car of garageStore[garage]) {
+      let desc = [];
+      for (const prop in car) {
+        desc.push(`${prop} - ${car[prop]}`);
       }
+      console.log(`--- ${desc.join(", ")}`);
     }
   }
-   
-  garage([
-    "17 - color: blue, fuel type: diesel",
-    "1 - color: red, manufacture: Audi",
-    "2 - fuel type: petrol",
-    "4 - color: dark blue, fuel type: diesel, manufacture: Fiat",
-  ]);
+}
+
+garage([
+  "17 - color: blue, fuel type: diesel",
+  "1 - color: red, manufacture: Audi",
+  "2 - fuel type: petrol",
+  "4 - color: dark blue, fuel type: diesel, manufacture: Fiat",
+]);
 
 garage([
   "1 - color: blue, fuel type: diesel",
