9a6a1239f65ad21b75c9b4201fb5dddf564dd77c
Marina Nenova <marinanenova89@gmail.com>
2023-03-14 16:28:50 +0200

    Solved Delete from Table exercise

--- JS-Front-end/DOM-and-Events/lab/05. Delete-From-Table/app.js
+++ JS-Front-end/DOM-and-Events/lab/05. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+  let emailElements = document.querySelectorAll("tr td:nth-of-type(2)");
+  let emailsArr = Array.from(emailElements);
+
+  let emailInput = document.querySelector('input[name="email"]').value;
+  let resultElement = document.getElementById("result");
+
+  let targetElement = emailsArr.find((x) => x.textContent === emailInput);
+
+  if (targetElement) {
+    targetElement.parentNode.remove();
+    resultElement.textContent = "Deleted.";
+  } else {
+    resultElement.textContent = "Not found.";
+  }
+  emailInput.value = "";
 }
