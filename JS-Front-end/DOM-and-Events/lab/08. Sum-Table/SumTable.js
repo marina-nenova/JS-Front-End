168ca73558d20067fc5623da111a872da6886c33
Marina Nenova <marinanenova89@gmail.com>
2023-03-14 16:10:53 +0200

    Solved Sum Table exercise

--- JS-Front-end/DOM-and-Events/lab/08. Sum-Table/SumTable.js
+++ JS-Front-end/DOM-and-Events/lab/08. Sum-Table/SumTable.js
@@ -1,3 +1,10 @@
 function sumTable() {
+  let prices = document.querySelectorAll("tr td:nth-of-type(2)");
+  let totalElement = document.getElementById("sum");
 
+  let sum = Array.from(prices).reduce((sum, price) => {
+    return sum + (Number(price.textContent) || 0);
+  }, 0);
+
+  totalElement.textContent = sum;
 }
