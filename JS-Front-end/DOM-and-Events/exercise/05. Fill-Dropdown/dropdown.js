61fb7c782fb3597abf770e3a846e41740a18e9c5
Marina Nenova <marinanenova89@gmail.com>
2023-03-15 16:40:18 +0200

    solved Fill Dropdown exercise

--- JS-Front-end/DOM-and-Events/exercise/05. Fill-Dropdown/dropdown.js
+++ JS-Front-end/DOM-and-Events/exercise/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemText = document.getElementById('newItemText');
+    let newitemvalue = document.getElementById('newItemValue');
+
+    let optionElement = document.createElement('option');
+    optionElement.textContent = newItemText.value;
+    optionElement.value = newitemvalue.value;
+
+    let selectelement = document.getElementById('menu');
+    selectelement.appendChild(optionElement);
+
+    newItemText.value = '';
+    newitemvalue.value = '';
 }
