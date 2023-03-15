694bd2b51329742edaad0c8894dbb19b46910522
Marina Nenova <marinanenova89@gmail.com>
2023-03-15 13:44:58 +0200

    refactorred list of items exercise

--- JS-Front-end/DOM-and-Events/lab/04. List-Of-Items/app.js
+++ JS-Front-end/DOM-and-Events/lab/04. List-Of-Items/app.js
@@ -1,10 +1,11 @@
 function addItem() {
     let ulElement = document.getElementById('items');
-    let newItem = document.getElementById('newItemText').value;
+    let newItem = document.getElementById('newItemText');
 
     let newLiItem = document.createElement('li');
-    newLiItem.textContent = newItem;
+    newLiItem.textContent = newItem.value;
 
     ulElement.appendChild(newLiItem);
+    newItem.value = '';
 
 }
