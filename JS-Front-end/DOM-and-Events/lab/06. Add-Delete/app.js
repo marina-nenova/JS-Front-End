108ef7d42f8004f420f09636bbf1ab058e3bbb11
Marina Nenova <marinanenova89@gmail.com>
2023-03-15 13:58:55 +0200

    Solved Add / Delete exercise

--- JS-Front-end/DOM-and-Events/lab/06. Add-Delete/app.js
+++ JS-Front-end/DOM-and-Events/lab/06. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    let ulElement = document.getElementById('items');
+    let newItem = document.getElementById('newItemText');
+
+    let newLiItem = document.createElement('li');
+    newLiItem.textContent = newItem.value;
+
+    let deleteLink = document.createElement('a');
+    deleteLink.textContent = '[Delete]';
+    deleteLink.href = '#';
+
+    deleteLink.addEventListener('click', (el => {
+        el.currentTarget.parentNode.remove();
+    }))
+
+    newLiItem.appendChild(deleteLink);
+    ulElement.appendChild(newLiItem);
+    newItem.value = '';
 }
