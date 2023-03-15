5ce6ae6888de4806f275f441ed574ca791e4814b
Marina Nenova <marinanenova89@gmail.com>
2023-03-15 15:28:12 +0200

    Solved Sections exercise

--- JS-Front-end/DOM-and-Events/exercise/02. Sections/script.js
+++ JS-Front-end/DOM-and-Events/exercise/02. Sections/script.js
@@ -1,3 +1,21 @@
 function create(words) {
-   console.log('TODO:...');
+  for (const word of words) {
+    let contentElement = document.getElementById("content");
+
+    let newDivElement = document.createElement("div");
+    let newParagraph = document.createElement("p");
+    newParagraph.textContent = word;
+    newParagraph.style.display = "none";
+
+    newDivElement.appendChild(newParagraph);
+
+    newDivElement.addEventListener("click", displayHandler);
+
+    function displayHandler(e) {
+      let paragraph = e.currentTarget.querySelector("p");
+      paragraph.style.display = "inline-block";
+    }
+
+    contentElement.appendChild(newDivElement);
+  }
 }
