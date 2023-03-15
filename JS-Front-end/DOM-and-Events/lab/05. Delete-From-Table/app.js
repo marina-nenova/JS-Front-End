d6e6bedeb60d2a730eff82248c500d13a1d812bf
Marina Nenova <marinanenova89@gmail.com>
2023-03-15 13:47:00 +0200

    refactorred delete from table exercise

--- JS-Front-end/DOM-and-Events/lab/05. Delete-From-Table/app.js
+++ JS-Front-end/DOM-and-Events/lab/05. Delete-From-Table/app.js
@@ -2,10 +2,10 @@ function deleteByEmail() {
   let emailElements = document.querySelectorAll("tr td:nth-of-type(2)");
   let emailsArr = Array.from(emailElements);
 
-  let emailInput = document.querySelector('input[name="email"]').value;
+  let emailInput = document.querySelector('input[name="email"]');
   let resultElement = document.getElementById("result");
 
-  let targetElement = emailsArr.find((x) => x.textContent === emailInput);
+  let targetElement = emailsArr.find((x) => x.textContent === emailInput.value);
 
   if (targetElement) {
     targetElement.parentNode.remove();
