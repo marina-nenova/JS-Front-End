a32eabe7412c505932fafe1ae1558bce33edb2d5
Marina Nenova <marinanenova89@gmail.com>
2023-03-15 15:44:12 +0200

    Solved Accordion exercise

--- JS-Front-end/DOM-and-Events/exercise/03. Accordion/accordion.js
+++ JS-Front-end/DOM-and-Events/exercise/03. Accordion/accordion.js
@@ -1,3 +1,20 @@
 function toggle() {
-    console.log('TODO:...');
+  let buttonElement = document.getElementsByClassName("button")[0];
+  let extraTextElement = document.getElementById("extra");
+
+  if (buttonElement.textContent === "More") {
+    buttonElement.addEventListener("click", showMore());
+    buttonElement.textContent = "Less";
+  } else {
+    buttonElement.addEventListener("click", showLess());
+    buttonElement.textContent = "More";
+  }
+
+  function showMore(e) {
+    extraTextElement.style.display = "block";
+  }
+
+  function showLess(e) {
+    extraTextElement.style.display = "none";
+  }
 }
