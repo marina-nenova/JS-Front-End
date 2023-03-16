fda6014aa7a41af7a743258dba35f130ab169c7f
Marina Nenova <marinanenova89@gmail.com>
2023-03-16 15:27:40 +0200

    Solved Edit Element exercise

--- JS-Front-end/DOM-and-Events/more-exercise/01. Edit-Element/EditElement.js
+++ JS-Front-end/DOM-and-Events/more-exercise/01. Edit-Element/EditElement.js
@@ -1,3 +1,7 @@
-function editElement() {
-    // TODO
+function editElement(element, match, replacement) {
+    let textElement = element.textContent;
+    while (textElement.includes(match)) {
+        textElement = textElement.replace(match, replacement);
+    }
+    element.textContent = textElement;
 }
