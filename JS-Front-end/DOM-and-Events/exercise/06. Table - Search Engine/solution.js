0905080c64db7160c6977c3a7417435b1e320490
Marina Nenova <marinanenova89@gmail.com>
2023-03-16 11:18:05 +0200

    Solved Table – Search Engine exercise

--- JS-Front-end/DOM-and-Events/exercise/06. Table - Search Engine/solution.js
+++ JS-Front-end/DOM-and-Events/exercise/06. Table - Search Engine/solution.js
@@ -2,7 +2,15 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let rowsElements = Array.from(document.querySelectorAll('.container > tbody tr'));
+      let searchedInput = document.getElementById('searchField');
 
+      for (const element of rowsElements) {
+         element.classList.remove('select')
+         if (searchedInput.value && element.textContent.toLowerCase().includes(searchedInput.value.toLowerCase())) {
+            element.className = 'select';
+         }
+      }
+      searchedInput.value = '';
    }
 }
