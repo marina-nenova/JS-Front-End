dbbde05749598f0b03b8e7c5ddd6e8aac88a4abe
Marina Nenova <marinanenova89@gmail.com>
2023-03-17 14:44:39 +0200

    Solved Pascal or Camel Case exercise

--- JS-Front-end/DOM-and-Events/more-exercise/06. Pascal-or-Camel-Case/solution.js
+++ JS-Front-end/DOM-and-Events/more-exercise/06. Pascal-or-Camel-Case/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-  //TODO...
+  let textInput = document.getElementById('text');
+  let namingConvention = document.getElementById('naming-convention');
+  let resultElement = document.getElementById('result');
+  
+  let wordsArr = textInput.value.split(' ').map(x => x.toLowerCase());
+  let result = '' 
+  
+  if(namingConvention.value === 'Camel Case') {
+    result = wordsArr[0];
+    for (const word of wordsArr.splice(1)) {
+      result += word[0].toUpperCase() + word.slice(1);
+    }
+    console.log(result);
+  } else if (namingConvention.value === 'Pascal Case') {
+    for (const word of wordsArr) {
+      result += word[0].toUpperCase() + word.slice(1);
+    }
+  } else {
+    result = "Error!";
+  }
+
+  resultElement.textContent = result;
 }
