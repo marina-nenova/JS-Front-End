79f6cf43fa580ee96d78227b6f7580dafdc9781c
Marina Nenova <marinanenova89@gmail.com>
2023-03-18 13:46:31 +0200

    Solved Encode and Decode Messages exercise

--- JS-Front-end/DOM-and-Events/more-exercise/12. Encode-and-Decode-Messages/app.js
+++ JS-Front-end/DOM-and-Events/more-exercise/12. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,28 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+  let [givenMessage, recievedMessage] = document.querySelectorAll("textarea");
+
+  let [encodeButton, decodeButton] = document.querySelectorAll("button");
+
+  encodeButton.addEventListener("click", () => {
+    let givenText = givenMessage.value;
+    let encodedMessage = "";
+    for (let i = 0; i < givenText.length; i++) {
+      let currentCharCode = givenText.charCodeAt(i);
+      let newChar = String.fromCharCode(currentCharCode + 1);
+      encodedMessage += newChar;
+    }
+    recievedMessage.value = encodedMessage;
+    givenMessage.value = "";
+  });
+
+  decodeButton.addEventListener("click", () => {
+    let receivedText = recievedMessage.value;
+    let decodedMessage = "";
+    for (let i = 0; i < receivedText.length; i++) {
+      let currentCharCode = receivedText.charCodeAt(i);
+      let newChar = String.fromCharCode(currentCharCode - 1);
+      decodedMessage += newChar;
+    }
+    recievedMessage.value = decodedMessage;
+  });
 }
