d0f732f6538b57355f163b4529b5401b65f00a73
Marina Nenova <marinanenova89@gmail.com>
2023-03-18 16:04:37 +0200

    Solved Sudomu exercise

--- JS-Front-end/DOM-and-Events/more-exercise/09. Generate Report/report.js
+++ JS-Front-end/DOM-and-Events/more-exercise/09. Generate Report/report.js
@@ -1,32 +1,74 @@
-function generateReport() {
-    let checkboxes = document.querySelectorAll('thead input[type="checkbox"]');
-    console.log(checkboxes);
-    let cbIndices = [];
+function solve() {
+  let [checkButton, clearButton] = document.querySelectorAll("button");
+  let allRowsElement = Array.from(document.querySelectorAll("tbody tr"));
+  let tableElement = document.querySelector("table");
+  let resultElement = document.querySelector("div p");
+  let isRowCorrect = true;
+  let isColCorrect = true;
 
-    for (let index = 0; index < checkboxes.length; index++) {
-        const element = checkboxes[index];
-        if (element.checked) {
-            cbIndices.push(index);
-        }
+  checkButton.addEventListener("click", onCheckClick);
+  clearButton.addEventListener("click", onClearClick);
+
+  function onCheckClick() {
+    let sudoku = createMatrix();
+    for (let i = 0; i < sudoku.length; i++) {
+      isRowCorrect = checkRow(sudoku, i);
+      isColCorrect = checkColumn(sudoku, i);
+
+      if (!isRowCorrect || !isColCorrect) {
+        console.log("false");
+        break;
+      }
     }
-
-    let rows = Array.from(document.querySelectorAll('tbody tr'));
-    
-    let result = [];
-
-    for (const row of rows) {
-        let info = {};
-
-        for (const index of cbIndices) {
-            let name = checkboxes[index].name;
-            let value = row.children[index].textContent;
-            info[name] = value;
-        }
-        result.push(info);
+    if (isRowCorrect && isColCorrect) {
+      tableElement.style.border = "2px solid green";
+      resultElement.textContent = "You solve it! Congratulations!";
+      resultElement.style.color = "green";
+    } else {
+      tableElement.style.border = "2px solid red";
+      resultElement.textContent = "NOP! You are not done yet...";
+      resultElement.style.color = "red";
     }
+  }
 
-    let report = JSON.stringify(result);
+  function onClearClick() {
+    let inputElements = document.querySelectorAll("td input");
+    tableElement.style = "none";
+    resultElement.textContent = "";
+    for (const input of inputElements) {
+      input.value = "";
+    }
+  }
 
-    let outputElement = document.getElementById('output');
-    outputElement.textContent = report;
+  function createMatrix() {
+    let matrix = [];
+    for (let row of allRowsElement) {
+      let rowArr = [];
+      for (let num of row.querySelectorAll("td input")) {
+        rowArr.push(num.value);
+      }
+      matrix.push(rowArr);
+    }
+    return matrix;
+  }
+
+  function checkRow(matrix, row) {
+    let correctRow = [1, 2, 3].join("");
+    let currentRow = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+      currentRow.push(matrix[row][i]);
+    }
+    return currentRow.sort().join("") === correctRow;
+  }
+
+  function checkColumn(matrix, col) {
+    let correctColumn = [1, 2, 3].join("");
+    let currentColumn = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+      currentColumn.push(matrix[i][col]);
+    }
+    return currentColumn.sort().join("") === correctColumn;
+  }
 }
--- JS-Front-end/DOM-and-Events/more-exercise/14. Sudomu/solution.js
+++ JS-Front-end/DOM-and-Events/more-exercise/14. Sudomu/solution.js
@@ -1,4 +1,26 @@
 function solve() {
 
-    //TODO...
+    let firstRow = document.querySelectorAll('tbody tr:nth-of-type(1) td');
+    let secondRow = document.querySelectorAll('tbody tr:nth-of-type(2) td');
+    let thirdRow = document.querySelectorAll('tbody tr:nth-of-type(3) td');
+
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+
+    for (const row of rows) {
+        let col = [];
+
+        for (let index=0; i < row.children.length; index++) {
+            col.push()
+        }
+        result.push(info);
+    }
+
+    let firstCol = [firstRow[0], secondRow[0], thirdRow[0]];
+    let secondCol = [firstRow[1], secondRow[1], thirdRow[1]];
+    let thirdCol = [firstRow[2], secondRow[2], thirdRow[2]];
+
+    console.log(firstCol);
+    console.log(secondCol);
+    console.log(thirdCol);
+    
 }
