4bb6e78bd93b023a1a56611eb49df27320e61750
Marina Nenova <marinanenova89@gmail.com>
2023-03-18 13:06:59 +0200

    Solved Generate Report exercise

--- JS-Front-end/DOM-and-Events/more-exercise/09. Generate Report/report.js
+++ JS-Front-end/DOM-and-Events/more-exercise/09. Generate Report/report.js
@@ -1,3 +1,32 @@
 function generateReport() {
-    //TODO
+    let checkboxes = document.querySelectorAll('thead input[type="checkbox"]');
+    console.log(checkboxes);
+    let cbIndices = [];
+
+    for (let index = 0; index < checkboxes.length; index++) {
+        const element = checkboxes[index];
+        if (element.checked) {
+            cbIndices.push(index);
+        }
+    }
+
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+    
+    let result = [];
+
+    for (const row of rows) {
+        let info = {};
+
+        for (const index of cbIndices) {
+            let name = checkboxes[index].name;
+            let value = row.children[index].textContent;
+            info[name] = value;
+        }
+        result.push(info);
+    }
+
+    let report = JSON.stringify(result);
+
+    let outputElement = document.getElementById('output');
+    outputElement.textContent = report;
 }
