aa288b7d3e80445b7d59d603a79daf6b7fbc78aa
Marina Nenova <marinanenova89@gmail.com>
2023-03-18 13:32:47 +0200

    Solved Number Convertor exercise

--- JS-Front-end/DOM-and-Events/more-exercise/10. Number Convertor/solution.js
+++ JS-Front-end/DOM-and-Events/more-exercise/10. Number Convertor/solution.js
@@ -1,4 +1,29 @@
 function solve() {
+  let toMenu = document.getElementById("selectMenuTo");
 
-    //TODO...
+  let binaryOption = document.createElement("option");
+  binaryOption.textContent = "Binary";
+  binaryOption.value = "binary";
+
+  let hexadecimalOption = document.createElement("option");
+  hexadecimalOption.textContent = "Hexadecimal";
+  hexadecimalOption.value = "hexadecimal";
+
+  toMenu.appendChild(binaryOption);
+  toMenu.appendChild(hexadecimalOption);
+
+  let convertBtn = document.querySelector("button");
+  convertBtn.addEventListener("click", convert);
+
+  function convert() {
+    let resultElement = document.getElementById("result");
+    let numberInput = document.getElementById("input");
+    let number = Number(numberInput.value);
+
+    if (toMenu.value === "binary") {
+      resultElement.value = number.toString(2);
+    } else if (toMenu.value === "hexadecimal") {
+      resultElement.value = number.toString(16).toUpperCase();
+    }
+  }
 }
