d33fef1d5341a468497fb6ecdfb5c44e2da6dc39
Marina Nenova <marinanenova89@gmail.com>
2023-03-22 11:41:19 +0200

    Solved Github Repos By Username exercise

--- JS-Front-end/HTTP-and-Rest/lab/05. Github-Repos-by-Username/scripts/app.js
+++ JS-Front-end/HTTP-and-Rest/lab/05. Github-Repos-by-Username/scripts/app.js
@@ -1,3 +1,17 @@
 function loadRepos() {
-	console.log("TODO...");
+	let usernameInput = document.getElementById('username').value; 
+	let resposList = document.getElementById('repos');
+	resposList.innerHTML = '';
+	fetch(`https://api.github.com/users/${usernameInput}/repos`)
+	.then(res => res.json())
+	.then(result => result.forEach(repo => {
+		let reposItem = document.createElement('li');
+		let repoLink = document.createElement('a');
+		repoLink.textContent = repo.full_name;
+		repoLink.href = repo.html_url;
+
+		reposItem.appendChild(repoLink);
+		resposList.appendChild(reposItem);
+	}))
+	.catch(err => console.log(err))
 }
