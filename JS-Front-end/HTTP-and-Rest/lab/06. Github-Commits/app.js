2c9805effe900b785f4831c63c329658dfeba861
Marina Nenova <marinanenova89@gmail.com>
2023-03-22 20:58:15 +0200

    Solved Github-Commits exercise

--- JS-Front-end/HTTP-and-Rest/lab/06. Github-Commits/app.js
+++ JS-Front-end/HTTP-and-Rest/lab/06. Github-Commits/app.js
@@ -1,4 +1,22 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+  let username = document.getElementById("username").value;
+  let repo = document.getElementById("repo").value;
+  let listItems = document.getElementById("commits");
+  listItems.innerHTML = "";
+
+  fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
+    .then((result) => result.json())
+    .then((res) => {
+        res.forEach((commitRes) => {
+        let item = document.createElement("li");
+        item.textContent = `${commitRes.commit.author.name}: ${commitRes.commit.message}`;
+        listItems.appendChild(item);
+      });
+    })
+    .catch((err) => {
+      console.error(err)
+      let item = document.createElement("li");
+      item.textContent = `Error: ${console.error(err)} (Not Found)`;
+      listItems.appendChild(item);
+    });
 }
