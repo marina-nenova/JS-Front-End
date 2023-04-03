dd3868c330adb3e3a4dc9aaeb2b39214528d893d
Marina Nenova <marinanenova89@gmail.com>
2023-04-03 18:00:56 +0300

    Solved Accordion exercise

--- JS-Front-end/HTTP-and-Rest/more-exercise/02. Accordion/accordion.js
+++ JS-Front-end/HTTP-and-Rest/more-exercise/02. Accordion/accordion.js
@@ -1,3 +1,62 @@
 function solution() {
-    //TODO .....
+  const articlesUrl = "http://localhost:3030/jsonstore/advanced/articles/list/";
+  const detailsUrl =
+    "http://localhost:3030/jsonstore/advanced/articles/details/";
+
+  let mainSection = document.getElementById("main");
+
+  fetch(articlesUrl)
+    .then((res) => res.json())
+    .then((result) => {
+      result.forEach((article) => {
+        let accordion = document.createElement("div");
+        accordion.classList.add("accordion");
+
+        let head = document.createElement("div");
+        head.classList.add("head");
+
+        let titleElement = document.createElement("span");
+        titleElement.textContent = article.title;
+        head.appendChild(titleElement);
+
+        let button = document.createElement("button");
+        button.classList.add("button");
+        button.id = article._id;
+        button.textContent = "MORE";
+        button.addEventListener("click", detailsHandler);
+        head.appendChild(button);
+
+        accordion.appendChild(head);
+        mainSection.appendChild(accordion);
+
+        let extraContainer = document.createElement("div");
+        extraContainer.classList.add("extra");
+        extraContainer.style.display = "none";
+        let detailsElement = document.createElement("p");
+        extraContainer.appendChild(detailsElement);
+        accordion.appendChild(extraContainer);
+
+        function detailsHandler(e) {
+          let currentArticle = e.currentTarget.parentElement.parentElement;
+          let extraContainer = currentArticle.querySelector(".extra");
+
+          if (e.currentTarget.textContent === "MORE") {
+            e.currentTarget.textContent = "LESS";
+            let pElement = extraContainer.querySelector("p");
+
+            fetch(`${detailsUrl}${this.id}`)
+              .then((res) => res.json())
+              .then((result) => {
+                pElement.textContent = result.content;
+                extraContainer.style.display = "block";
+              });
+          } else {
+            e.currentTarget.textContent = "MORE";
+            extraContainer.style.display = "none";
+          }
+        }
+      });
+    });
 }
+
+solution();
